157ca6da1ddb778ca01dafd96036888e92f17d4a
batraktar <maks.batrak1222@gmail.com>
2023-04-14 01:59:49 +0300

    first commit in my project| hm_1

---
+++ mysite/myapp/views.py
@@ -0,0 +1,45 @@
+from django.http import HttpResponse
+
+
+def blogs(request):
+    return HttpResponse('your blogs')
+
+
+def about(request):
+    return HttpResponse('about')
+
+
+def cip(request, name):
+    return HttpResponse(f"comment for post {name}")
+
+
+def ufp(request, name):
+    return HttpResponse(f"update post {name}")
+
+
+def dp(request, name):
+    return HttpResponse(f"delete post {name}")
+
+
+def profile(request, username):
+    return HttpResponse(f'Profile {username}')
+
+
+def change_password(request):
+    return HttpResponse('Change Password')
+
+
+def register(request):
+    return HttpResponse('register')
+
+
+def login(request):
+    return HttpResponse('log in')
+
+
+def logout(request):
+    return HttpResponse('log out')
+
+
+def blog_post(request, name):
+    return HttpResponse(f'Your post {name}')
---
+++ mysite/mysite/urls.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from django.urls import path
+from myapp.views import blogs, about, blog_post, cip, ufp, dp, change_password, login, profile, register, logout
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', blogs, name='blogs'),
+    path('blogs/', blogs, name='blogs'),
+    path('about/', about, name='about'),
+    path('', blogs, name='blogs'),
+    path('login/', login, name='login'),
+    path('logout/', logout, name='logout'),
+    path('profile/<str:username>/', profile, name='profile'),
+    path('change_password/', change_password, name='change_password'),
+    path('register/', register, name='register'),
+    path('<slug:name>/', blog_post, name='blog_post'),
+    path('<slug:name>/comment/', cip, name='comment_in_post'),
+    path('<slug:name>/update/', ufp, name='update_for_post'),
+    path('<slug:name>/delete/', dp, name='delete_post'),
+]
+
+
